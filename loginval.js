4005122161a40872932274c7ee73f08599b396e7
dungeonaltaf <altafsofficial01@gmail.com>
2018-07-21 21:08:05 +0530

    Update loginval.js

--- loginval.js
+++ loginval.js
@@ -1,67 +1,105 @@
-setTimeout(function () {
-	$('.email > input').focus();
+$(document).ready(function()
+{
+    setTimeout(function () {
+    $('.email > input').focus();
 }, 300);
 
 $('.email > input').on('keydown', function (event) 
 {
-	if (event.which === 13 || event.keyCode === 13) 
-	{
-		$('.email > input').blur();
-		$('.next').click();
-	}
+    if (event.which === 13 || event.keyCode === 13) 
+    {
+        $('.email > input').blur();
+        $('.next').click();
+    }
 });
 
 $('.password > input').on('keydown', function (event) {
-	if (event.which === 13 || event.keyCode === 13) {
-		$('.login').click();
-	}
+    if (event.which === 13 || event.keyCode === 13) {
+        $('.login').click();
+    }
 });
 
-$('.next').on('click', function (event) {
-	var emailInput = $('.email > input').val();
-	if (validateEmail(emailInput)) 
-	{
-		event.preventDefault();
-		$('.inputs').addClass('shift');
-		$('.back').addClass('active-back');
-		$('.email > input').css({
-			'border': '1px solid #cccccc'
-		});
-		$('.warning').empty();
-		setTimeout(function () {
-			$('.password > input').focus();
-		}, 400);
-	} 
-	else 
-	{
-		event.preventDefault();
-		$('.warning').empty();
-		$('.email > input').css({
-			'border': '1px solid red'
-		});
-		$('.warning').append('Invalid Email Address');
-	}
+
+$(".next").on('click' ,function(event)
+{
+	  var email = $("#email").val();
+      if ($.trim(email)==0)
+      {
+        alert("Please enter your email or username");
+        event.preventDefault();
+
+      }
+      else
+      {
+       event.preventDefault();
+        $('.inputs').addClass('shift');
+        $('.back').addClass('active-back');
+        $('.email > input').css({
+            'border': '1px solid #cccccc'
+        });
+        $('.warning').empty();
+        setTimeout(function () {
+            $('.password > input').focus();
+        }, 400);
+
+
+      }
 });
 
-$('.back').on('click', function (event) {
-	event.preventDefault();
-	$('.inputs').removeClass('shift');
-	$('.back').removeClass('active-back');
-	setTimeout(function () {
-		$('.email > input').focus();
-	}, 300);
+
+$('.back').on('click', function (event) 
+{
+    event.preventDefault();
+    $('.inputs').removeClass('shift');
+    $('.back').removeClass('active-back');
+    setTimeout(function () {
+        $('.email > input').focus();
+    }, 300);
 });
 
 $('.login').on('click', function (event) {
-	event.preventDefault();
-	$('form').empty();
-	$('form').append('<div class="loader"></div>');
-	setTimeout(function () {
-		location = location;
-	}, 2000);
-});
+    event.preventDefault();
+    var email = $("#email").val();
+    var password =$("#password").val();
+        if (password=="")
+    {
+        alert("Please input your password ");
+    }
+    else
+    {
+        $('form').empty();
+        $('form').append('<div class="loader"></div>');
+        setTimeout(function () 
+        {
+           location = location;
+        }, 2000);
+     
+     $.ajax(
+     {
+         url:'loginval.php',
+         method:'POST',
+         data: {
+                 login:'1',
+                 email1:email,
+                 password1:password
 
-var validateEmail = function validateEmail(email) {
-	var re = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
-	return re.test(email);
-};
+         },
+         success: function(response){
+
+            $("#response").html(response);
+         },
+
+         dataType:'text'
+          
+     
+
+
+     });
+
+
+    }
+    
+
+});
+      
+});
