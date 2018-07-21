1c1b471cfeb13510fe353529b8f5ed45da3c294e
dungeonaltaf <altafsofficial01@gmail.com>
2018-07-21 19:44:40 +0530

    Add files via upload

--- loginval.js
+++ loginval.js
@@ -1,36 +1,67 @@
-$(document).ready(function(){
-$("#login").on('click' ,function(){
-	
-var email = $("#email").val();
-var password =$("#password").val();
-console.log(password);
+setTimeout(function () {
+	$('.email > input').focus();
+}, 300);
 
-if (email=="" || password=="")
-alert("Please fill the eamil and password");
-else
+$('.email > input').on('keydown', function (event) 
 {
-	$.ajax({
-         url:'loginval.php',
-         method:'POST',
-         data: {
-                 login:'1',
-                 email1:email,
-                 password1:password
-
-         },
-         success: function(response){
-
-         	$("#response").html(response);
-         },
-
-         dataType:'text'
-   
-
-
-     });
-}
-
-
+	if (event.which === 13 || event.keyCode === 13) 
+	{
+		$('.email > input').blur();
+		$('.next').click();
+	}
 });
 
+$('.password > input').on('keydown', function (event) {
+	if (event.which === 13 || event.keyCode === 13) {
+		$('.login').click();
+	}
 });
+
+$('.next').on('click', function (event) {
+	var emailInput = $('.email > input').val();
+	if (validateEmail(emailInput)) 
+	{
+		event.preventDefault();
+		$('.inputs').addClass('shift');
+		$('.back').addClass('active-back');
+		$('.email > input').css({
+			'border': '1px solid #cccccc'
+		});
+		$('.warning').empty();
+		setTimeout(function () {
+			$('.password > input').focus();
+		}, 400);
+	} 
+	else 
+	{
+		event.preventDefault();
+		$('.warning').empty();
+		$('.email > input').css({
+			'border': '1px solid red'
+		});
+		$('.warning').append('Invalid Email Address');
+	}
+});
+
+$('.back').on('click', function (event) {
+	event.preventDefault();
+	$('.inputs').removeClass('shift');
+	$('.back').removeClass('active-back');
+	setTimeout(function () {
+		$('.email > input').focus();
+	}, 300);
+});
+
+$('.login').on('click', function (event) {
+	event.preventDefault();
+	$('form').empty();
+	$('form').append('<div class="loader"></div>');
+	setTimeout(function () {
+		location = location;
+	}, 2000);
+});
+
+var validateEmail = function validateEmail(email) {
+	var re = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+	return re.test(email);
+};
