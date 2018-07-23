6534953be2fafc90d03359a1712683e0b6d4182e
dungeonaltaf <altafsofficial01@gmail.com>
2018-07-23 11:57:19 +0530

    Update login.php

--- login/login.php
+++ login/login.php
@@ -37,7 +37,7 @@
   src="https://code.jquery.com/jquery-3.3.1.js"
   integrity="sha256-2Kok7MbOyxpgUVvAk/HJ2jigOSYS2auK4Pfzbm7uH60="
   crossorigin="anonymous"></script>
-<script src = "loginval1.js"></script>
+<script src = "loginval.js"></script>
 
 
 </body>
